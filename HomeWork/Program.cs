d0a360b4aefa0baeb9313794ef7ad2452dcc73ab
NVitaly <whiskalik@gmail.com>
2022-09-07 19:33:27 +0300

    Задача 15

--- HomeWork/Program.cs
+++ HomeWork/Program.cs
@@ -15,17 +15,31 @@
 
 //Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
 
-Console.WriteLine("Введите число");
-int num = Convert.ToInt32 (Console.ReadLine());
-void numbers(int num)
+// Console.WriteLine("Введите число");
+// int num = Convert.ToInt32 (Console.ReadLine());
+// void numbers(int num)
+// {
+//     if (num >= 100)
+//     {
+//         Console.WriteLine("Третья цифра "+num.ToString()[2]);
+//     }
+//         else if (num <= 99)
+//     {
+//         Console.WriteLine("Третьей цифры нет");
+//     }
+// }
+//     numbers(num);
+
+//Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+
+Console.Write("Введите число дня недели ");
+int day = Convert.ToInt32 (Console.ReadLine());
+void number_day (int day)
 {
-    if (num >= 100)
-    {
-        Console.WriteLine("Третья цифра "+num.ToString()[2]);
-    }
-        else if (num <= 99)
-    {
-        Console.WriteLine("Третьей цифры нет");
-    }
+    if (day <= 5)
+    Console.WriteLine("Не является выходным");
+    else if (day <=7)
+    Console.WriteLine("Выходной");
+    else Console.WriteLine("Вы вводите недопустимое значение число "+day);
 }
-    numbers(num);
+number_day(day);
