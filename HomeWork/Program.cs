e0478704b5a523ee4a39cdfcbf53daba43b48f51
NVitaly <whiskalik@gmail.com>
2022-09-07 19:30:58 +0300

    Задача 13

--- HomeWork/Program.cs
+++ HomeWork/Program.cs
@@ -1,14 +1,31 @@
 ﻿//Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
 
-Console.WriteLine("Введите любое трёхзначное число ");
-try
-{ 
-    int num = Convert.ToInt32 (Console.ReadLine());
-    int num1 = num % 100;
-    int num2 = num1 / 10;
-    Console.WriteLine(num2);
-}
-catch 
+// Console.WriteLine("Введите любое трёхзначное число ");
+// try
+// { 
+//     int num = Convert.ToInt32 (Console.ReadLine());
+//     int num1 = num % 100;
+//     int num2 = num1 / 10;
+//     Console.WriteLine(num2);
+// }
+// catch 
+// {
+//     Console.WriteLine("Введены некорректные данные");
+// }
+
+//Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+Console.WriteLine("Введите число");
+int num = Convert.ToInt32 (Console.ReadLine());
+void numbers(int num)
 {
-    Console.WriteLine("Введены некорректные данные");
+    if (num >= 100)
+    {
+        Console.WriteLine("Третья цифра "+num.ToString()[2]);
+    }
+        else if (num <= 99)
+    {
+        Console.WriteLine("Третьей цифры нет");
+    }
 }
+    numbers(num);
